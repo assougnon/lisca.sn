9761fdcbac1e6b7367c2f46a72b96eea53bd42d6
Leon legrand ASSOUGNON <assougnonleon@gmail.com>
2019-10-26 08:32:22 +0000

    secondCommit

--- resources/views/hopital/index.blade.php
+++ resources/views/hopital/index.blade.php
@@ -4,7 +4,7 @@
 
         <div class="card shadow mb-12">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Liste des des  médicaments </h6>
+                <h6 class="m-0 font-weight-bold text-primary">Liste des   médicaments </h6>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
